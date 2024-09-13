a18872aa1bd4ae46b2ba408c2bb337c9203b4e06
Sahil Suthar <100418805+SahilS-26@users.noreply.github.com>
2024-09-13 14:45:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ export default function App() {
         </Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} key={selectedFriend.id} />
       )}
     </div>
   );
